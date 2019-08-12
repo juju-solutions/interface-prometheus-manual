7608cd97909e13d43eeccb26d2d76af2ad3f18d2
Cory Johns <johnsca@gmail.com>
2019-08-12 10:27:30 -0400

    Refactor ca_file writing logic out of interface layer into charm

--- common.py
+++ common.py
@@ -1,7 +1,5 @@
 import json
 from copy import deepcopy
-from pathlib import Path
-from hashlib import sha1
 
 from charms.reactive import BaseRequest, BaseResponse, Field
 
@@ -21,40 +19,33 @@ class JobRequest(BaseRequest):
 
     ca_cert = Field('Cert data for the CA used to validate connections.')
 
-    def to_json(self):
+    def to_json(self, ca_file=None):
         """
         Render the job request to JSON string which can be included directly
         into Prometheus config.
 
         Keys will be sorted in the rendering to ensure a stable ordering for
         comparisons to detect changes.
+
+        If `ca_file` is given, it will be used to replace the value of any
+        `ca_file` fields in the job.  The charm should ensure that the
+        request's `ca_cert` data is writen to that path prior to calling this
+        method.
         """
         job_data = deepcopy(self.job_data)  # make a copy we can modify
         job_data['job_name'] = '{}-{}'.format(self.job_name, self.request_id)
 
-        if self.ca_cert:
-            # cert has to be provided in a file, so we need to stash it on disk
-            # FIXME: this should probably be provided by the charm, but it also
-            # needs to be populated into the JSON data
-            cert_dir = Path('/var/snap/prometheus/common/certs')
-            cert_dir.mkdir(parents=True, exist_ok=True)
-
-            # use a hash of the cert data rather than the request ID so that we
-            # can detect changes to the data via the filename
-            cert_hash = sha1(self.ca_cert.encode('utf8')).hexdigest()
-            cert_path = cert_dir / '{}-{}.crt'.format(self.job_name, cert_hash)
-            cert_path.write_text(self.ca_cert + '\n')
-
+        if ca_file:
             for key, value in job_data.items():
                 # update the cert path at the job level
                 if key == 'tls_config':
-                    value['ca_file'] = str(cert_path)
+                    value['ca_file'] = str(ca_file)
 
                 # update the cert path at the SD config level
                 if key.endswith('_sd_configs'):
                     for sd_config in value:
                         if 'ca_file' in sd_config.get('tls_config', {}):
-                            sd_config['tls_config']['ca_file'] = str(cert_path)
+                            sd_config['tls_config']['ca_file'] = str(ca_file)
 
         return json.dumps(job_data, sort_keys=True)
 
